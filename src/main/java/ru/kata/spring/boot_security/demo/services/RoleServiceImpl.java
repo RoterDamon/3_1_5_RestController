d82aea742fc7a3fb7225af8f733729bd093f8e5d
RoterDamon <80481177+RoterDamon@users.noreply.github.com>
2023-02-01 16:19:26 +0300

    edits

---
+++ src/main/java/ru/kata/spring/boot_security/demo/services/RoleServiceImpl.java
@@ -0,0 +1,23 @@
+package ru.kata.spring.boot_security.demo.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.kata.spring.boot_security.demo.models.Role;
+import ru.kata.spring.boot_security.demo.repository.RoleRepository;
+
+@Service
+@Transactional
+public class RoleServiceImpl implements RoleService {
+    private final RoleRepository roleRepository;
+
+    @Autowired
+    public RoleServiceImpl(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
+
+    @Override
+    public Role getRoleByName(String name) {
+        return roleRepository.findByName(name);
+    }
+}
